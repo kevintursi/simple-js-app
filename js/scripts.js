b07e51e9fb5ec0457af30a110ffebea4aa5438c4
kevintursi <kjt923@gmail.com>
2022-08-26 16:41:45 -0400

    Update scripts.js for line indentation

--- js/scripts.js
+++ js/scripts.js
@@ -29,8 +29,8 @@ let pokemonList = [
 
  for (let i = 0; i < pokemonList.length; i++) {
   if (pokemonList[i].height > 1.0){
-    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') - Wow, that\'s big! ')
+    document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') - Wow, that\'s big! ')
   }else if (pokemonList[i].height){
-    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') ')
+    document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') ')
   }
 }
