6b9e56916802ab93b4913fe49306868ac0a4e656
kevintursi <kjt923@gmail.com>
2022-09-08 16:44:33 -0400

    Changed for loop to forEach loop

--- js/scripts.js
+++ js/scripts.js
@@ -25,12 +25,16 @@ let pokemonList = [
   }
 ];
 
-//Loop that iterates Pokemon's names and heights and comments on biggest Pokemon
 
-for (let i = 0; i < pokemonList.length; i++) {
-  if (pokemonList[i].height > 1.0){
-    document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') - Wow, that\'s big! ') //For Pokemon who's height is above 1.0 to call them big
-  }else if (pokemonList[i].height){
-    document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') ') //For Pokemon who are not above 1.0
+// height treshold for guaging whether a pokemon is big or not
+let heightThresh = 1.0;
+
+// forEach loop to iterate each pokemon in the pokemonList
+pokemonList.forEach(function(pokemon) {
+  //for pokemon equal to or above the height treshold to be labeled "Wow, that's big!"
+  if (pokemon.height >= heightThresh) {
+    document.write(`<li>${pokemon.name} (height: ${pokemon.height}m) - Wow, that's big!</li>`);
+  } else {
+    document.write(`<li>${pokemon.name} (height: ${pokemon.height}m)</li>`);
   }
-}
+});
