d8bea6041174d516d11730a078831bea71bd8683
kevintursi <kjt923@gmail.com>
2022-09-29 16:09:42 -0400

    Added Bootstrap Elements

--- js/scripts.js
+++ js/scripts.js
@@ -68,70 +68,14 @@ let pokemonRepository = (function () {
     });
   };
 
-//Modal to display Pokemon's name, height, and an image
-  function showModal(pokemon) {
-    let modalContainer = document.querySelector('.pokemon-modal');
-    modalContainer.innerText = '';
-
-    let modal = document.createElement('div');
-    modal.classList.add('modal');
-
-    let closeButton = document.createElement('button');
-
-    closeButton.classList.add('modal-close');
-    closeButton.innerText = 'Close';
-
-    closeButton.addEventListener('click', hideModal);
-
-    let title = document.createElement('h1');
-    title.innerText = pokemon.name;
-
-    let height = document.createElement('p');
-    height.innerText = 'Height: ' + pokemon.height + 'm';
-
-    let image = document.createElement('img');
-    image.src = pokemon.imageUrl;
-
-    modal.appendChild(title);
-    modal.appendChild(height);
-    modal.appendChild(image);
-    modal.appendChild(closeButton);
-    modalContainer.appendChild(modal);
-
-
-
-    //lets user hide modal by pressing in modal container
-    modalContainer.addEventListener('click', (e) => {
-      let target = e.target;
-      if (target === modalContainer) {
-        hideModal();
-      }
-    })
-
-    modalContainer.classList.add('is-visible');
-
-  }
-
-    function hideModal() {
-      let modalContainer = document.querySelector('.pokemon-modal')
-      modalContainer.classList.remove('is-visible');
-    }
-
-    //lets user hide modal by pressing 'Escape' key
-    window.addEventListener('keydown', (e) => {
-      let modalContainer = document.querySelector('.pokemon-modal');
-      if(e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
-        hideModal();
-      }
-    });
-
 // returning oject with getAll() and add(pokemon) and addListItem() assigned as keys
   return {
     add: add,
     getAll: getAll,
     loadList: loadList,
     loadDetails: loadDetails,
-    addListItem: addListItem
+    addListItem: addListItem,
+    showDetails: showDetails
   };
 })();
 
